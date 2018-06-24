453b47752a9c720ec5873032a54383e0a21860b1
Mateus Esteves <mateus.esteves1994@gmail.com>
2018-06-24 16:08:43 -0300

    Desafios 20 e 21

--- desafio-01.js
+++ desafio-01.js
@@ -17,6 +17,17 @@ function sortear(numero) {
     return Math.floor(Math.random() * numero);
 }
 
-console.log(media(10, 24, 55, 90));
-console.log(mediaArredondada(987, 240, 282, 134.23));
-console.log(sortear(10));
+function aprovadoOuReprovado() {
+    let valores = Array.from(arguments);
+    let reprovado = valores.some(valor => valor === 0) || mediaArredondada(...arguments) < 60;
+
+    return reprovado ? 'Reprovado' : 'Aprovado';
+}
+
+console.log('Média seimples:', media(10, 24, 55, 90));
+console.log('Média arredondada:', mediaArredondada(987, 240, 282, 134.23));
+console.log('Sortear:', sortear(10));
+
+console.log('Aluno Aprovado:', aprovadoOuReprovado(90, 45, 80, 75, 99, 78));
+console.log('Aluno Reprovado (1 nota zero):', aprovadoOuReprovado(0, 45, 80, 75, 99, 78));
+console.log('Aluno Reprovado (abaixo de 60):', aprovadoOuReprovado(10, 45, 20, 35, 2, 32));
