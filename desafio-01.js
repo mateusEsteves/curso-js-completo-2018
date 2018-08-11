e981711d5795de28b658e2358a95abda6119e8dc
Mateus Esteves <mateus.esteves1994@gmail.com>
2018-08-11 20:08:49 -0300

    Desafios 30 e 31

--- desafio-01.js
+++ desafio-01.js
@@ -1,12 +1,9 @@
 function media() {
-    let somatorio = 0.0;
-    
-    for (let i = 0; i < arguments.length; i++){
-        somatorio += arguments[i];
-    }
+    let somatorio = Array.prototype.reduce.call(arguments, function (acumulador, currentValue) {
+        return acumulador + currentValue; 
+    }, 0);
 
-    let media = somatorio / arguments.length;
-    return media;
+    return somatorio / arguments.length;
 }
 
 function mediaArredondada() {
@@ -24,10 +21,34 @@ function aprovadoOuReprovado() {
     return reprovado ? 'Reprovado' : 'Aprovado';
 }
 
-console.log('Média seimples:', media(10, 24, 55, 90));
+Array.prototype.getMedia = function () {
+    return media(...this);
+}
+
+String.prototype.replaceAll = function (searchValue, replaceValue) {
+    let _string = this;
+    
+    do {
+        _string = _string.replace(searchValue, replaceValue);
+    } while (_string.includes(searchValue));
+    
+    return _string;
+}
+
+console.log('Média simples:', media(10, 24, 55, 90));
 console.log('Média arredondada:', mediaArredondada(987, 240, 282, 134.23));
 console.log('Sortear:', sortear(10));
+
+console.log('---------------------');
 
 console.log('Aluno Aprovado:', aprovadoOuReprovado(90, 45, 80, 75, 99, 78));
 console.log('Aluno Reprovado (1 nota zero):', aprovadoOuReprovado(0, 45, 80, 75, 99, 78));
 console.log('Aluno Reprovado (abaixo de 60):', aprovadoOuReprovado(10, 45, 20, 35, 2, 32));
+
+console.log('---------------------');
+
+console.log('Array.prototype.getMedia(): ', [1, 2, 3, 4].getMedia());
+
+console.log('---------------------');
+
+console.log('String.prototype.replaceAll(): ', 'aabbccddaattssjjaa'.replaceAll('aa', '##'));
